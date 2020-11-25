809ae1af7fd02efdf21a25622d6eda3698dd630f
fay <869770665@qq.com>
2020-11-25 19:06:50 +0800

    upd

--- front-end/dist/m/static/js/seo.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?79fc3112d44425ae09db65c527978fb7";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s); 
-})();
